39a4d82f16fd0e91269adc397f09ee622d69c189
Maksym <mvsozykin@gmail.com>
2024-11-22 14:12:30 +0200

    MovieCast component updated

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,11 +1,15 @@
 import { fetchMovieCreditsById } from '../../services/api';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import css from './MovieCast.module.css';
 
 const MovieCast = () => {
   const { movieId } = useParams();
-
   const [casts, setCats] = useState([]);
+
+  const defaultImg =
+    'https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster';
+
   useEffect(() => {
     if (!movieId) return;
     const getMovie = async () => {
@@ -18,17 +22,19 @@ const MovieCast = () => {
   return casts.length === 0 ? (
     <p>There is no information about the actors!</p>
   ) : (
-    <ul>
+    <ul className={css['cast-list']}>
       {casts.cast.map(cast => (
-        <li key={cast.credit_id}>
-          <div>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${cast.profile_path}`}
-              alt={cast.name}
-            />
-            <p>{cast.name}</p>
-            <p>Character: {cast.character}</p>
-          </div>
+        <li key={cast.credit_id} className={css['cast-item']}>
+          <img
+            src={
+              cast.profile_path
+                ? `https://image.tmdb.org/t/p/w500/${cast.profile_path}`
+                : defaultImg
+            }
+            alt={cast.name}
+          />
+          <p>{cast.name}</p>
+          <p>{cast.character}</p>
         </li>
       ))}
     </ul>
