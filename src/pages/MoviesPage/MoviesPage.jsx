f8b04d5b98497819130ed22ac42355fa01f5374b
Maksym <mvsozykin@gmail.com>
2024-11-22 19:41:57 +0200

    MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -6,7 +6,7 @@ import { MovieList } from '../../components/MovieList/MovieList';
 import Loader from '../../components/Loader/Loader';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import { fetchMovies } from '../../services/api';
-import css from './MoviesPages.module.css';
+import css from './MoviesPage.module.css';
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
   const [queryParams, setQueryParams] = useSearchParams();
